49f7646ae5623765e112ba0fedec3e5a6cc58924
Vaibhav Bura <vaibhavbura@gmail.com>
2025-04-04 00:27:54 +0530

    Navbar & Authentication

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+            <h2>Over 1000+ jobs to apply</h2>
+            <p>Explore jobs at india remote </p>
+            <div>
+                <div>
+                    <img src={assets.search_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
