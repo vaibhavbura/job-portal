c164e46dedea36e92f31d50d7cb717dd0c5b37ba
Vaibhav Bura <vaibhavbura@gmail.com>
2025-04-06 11:36:00 +0530

    pagination

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -101,7 +101,7 @@ const JobListing = () => {
 
             <div className="flex items-center justify-center space-x-2 mt-10">
                 <a href="job-list">
-                    <img src={assets.left_arrow_icon} alt="" />
+                    <img onClick={()=> setCurrentPage(Math.max(currentPage-1),1)} src={assets.left_arrow_icon} alt="" />
                 </a>
                 {Array.from({length:Math.ceil(jobs.length/6)}).map((_,index)=>(
                     <a href="#job-list">
@@ -109,7 +109,7 @@ const JobListing = () => {
                     </a>
                 ))}
                 <a href="#job-list">
-                    <img src={assets.right_arrow_icon} alt="" />
+                    <img onClick={()=> setCurrentPage(Math.min(currentPage+1,Math.ceil(jobs.length/6)))} src={assets.right_arrow_icon} alt="" />
                 </a>
             </div>
           )}
