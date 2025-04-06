fdd2a6c744cdcc353277b59dd35a94c9baeb944a
Vaibhav Bura <vaibhavbura@gmail.com>
2025-04-06 14:19:20 +0530

    loading effect

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -160,7 +160,7 @@ const JobListing = () => {
             </a>
             {Array.from({ length: Math.ceil(filteredJobs.length / 6) }).map(
               (_, index) => (
-                <a href="#job-list">
+                <a key={index} href="#job-list">
                   <button
                     onClick={() => setCurrentPage(index + 1)}
                     className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${
--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import { jobsData } from '../assets/assets'
+import Loading from '../components/Loading'
 const ApplyJob = () => {
-  return (
-    <div>ApplyJob</div>
+
+  const { id } = useParams()
+
+  const [JobData,setJobData] = useState(null)
+
+  const { jobs } = useContext(AppContext)
+
+  const fetchJob = async () =>{
+    const data = jobs.filter( job => job._id === id )
+    if(data.length!==0){
+      setJobData(data[0])
+      console.log(data[0]);
+    }
+  }
+
+  useEffect(()=>{
+    if(jobs.length >0){
+      fetchJob()
+    }
+    
+  },[id,jobs])
+  return JobData ? (
+    <div>
+
+    </div>
+  ) :(
+    <Loading/>
   )
 }
 
